3498ffc7d192e0e50f395f0bedb107ded8d38c52
Pulat Shodmanov <pshodmanovv@gmail.com>
2024-06-28 14:18:20 +0500

    jh

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -1,17 +1,16 @@
 from rest_framework import serializers
 from .models import Post
-from django.db import models
-from datetime import datetime
 
 class PostSerializer(serializers.ModelSerializer):
+    created_format = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
         fields = ['id', 'categorys', 'name', 'img', 'img2', 'text', 'text2', 'created']
         ordering = ('-created',)
 
-    @property   
-    def created_format(self):
-        return self.created.strftime('%b %d %Y')
+    def get_created_format(self, obj):
+        return obj.created.strftime('%b %d %Y')
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
